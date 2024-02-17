340452b819cf65ec20f327bf8a1bc54b26ee11de
Simon Rask <33556894+SimonRask@users.noreply.github.com>
2024-02-17 09:58:37 +0100

    Update keymap.c

--- config/keymap.c
+++ config/keymap.c
@@ -10,7 +10,6 @@
 #define LEFT 2
 #define RIGHT 3
 #define MISC 4
-#define FUNCTION 5
 #define ARROWS 6
 
 
@@ -21,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
       * │ESC│ A │ S │ D │ F │ G │           │ H │ J │ K │ L │ Æ │ Ø │
       * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
-      * │Sft│ Z │ X │ C │ V │ B │           │ N │ M │ , │ . │ - │Sft│
+      * │Sft│ Z │ X │ C │ V │ B │           │ N │ M │ , │ . │ - │WIN│
       * └───┴───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┴───┘
       *          ┌───┐                               ┌───────┐
       *          │Alt├─────┐                   ┌─────┤GUI/Bsp│
@@ -50,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                    └────┘      └───────┘
       */
     [ARROWS] = LAYOUT_split_3x6_3(
-        _______, _______, _______,  _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                                 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______,  _______, _______,
-                                        _______, _______, _______,                     _______,  _______, _______ 
+        _______, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                     KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,
+        KC_CAPS, _______, _______, _______, KC_VOLU, KC_DEL,                                 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_INS, KC_F12,
+        _______, _______, _______, KC_MUTE, KC_VOLD, _______,                                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,
+                                        KC_MPRV, KC_MPLY, KC_MNXT,                     _______,  _______, _______ 
     ),
      /*
       * ┌───┬───┬───┬───┬─────┬───┐       ┌───┬───┬───┬───┬───┬───┐
@@ -61,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ├───┼───┼───┼───┼─────┼───┤       ├───┼───┼───┼───┼───┼───┤
       * │   │ @ │ = │ ! │LEFT │ ~ │       │ # │ & │ " │ | │ ' │   │
       * ├───┼───┼───┼───┼─────┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ ? │ % │ ^ │RIGHT│ * │       │ + │ $ │ ; │ : │   │   │
+      * │   │ ? │ % │ ^ │RIGHT│ * │       │ + │ $ │   │   │   │   │
       * └───┴───┴───┴───┴─────┴───┘       └───┴───┴───┴───┴───┴───┘
       *           ┌────┐                        ┌────┐
       *           │    ├────┐              ┌────┤MISC│
@@ -70,18 +69,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                     └────┘   └─────┘
       */
     [SYMBOL] = LAYOUT_split_3x6_3(
-        XXXXXXX,  DK_1,     DK_2,     DK_3,     DK_4,    DK_5,                     DK_6, DK_7,    DK_8,    DK_9,    DK_0,    XXXXXXX,
-        XXXXXXX, DK_AT,  DK_EQL,  DK_EXLM,  OSL(LEFT),   DK_TILD,                  DK_HASH, DK_AMPR, DK_DQUO, DK_PIPE, DK_QUOT, XXXXXXX,
-        XXXXXXX, DK_QUES,  DK_PERC,  DK_CIRC,  OSL(RIGHT),  DK_ASTR,                DK_PLUS, DK_DLR, DK_SCLN, DK_COLN, XXXXXXX, XXXXXXX,
-                                        TO(SYMBOL), TO(BASE), XXXXXXX,         XXXXXXX, KC_BSPC, TO(MISC)
+        _______, DK_1,     DK_2,     DK_3,     DK_4,    DK_5,                     DK_6, DK_7,    DK_8,    DK_9,    DK_0,    _______,
+        _______, DK_AT,  DK_EQL,  DK_EXLM,  OSL(LEFT),   DK_TILD,                  DK_HASH, DK_AMPR, DK_DQUO, DK_PIPE, DK_QUOT, _______,
+        _______, DK_QUES,  DK_PERC,  DK_CIRC,  OSL(RIGHT),  DK_ASTR,                DK_PLUS, DK_DLR, _______, _______, _______, _______,
+                                        TO(MISC), TO(BASE), _______,         _______, KC_BSPC, TO(SYMBOL)
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
       * │   │   │   │   │   │   │       │   │   │   │   │ ( │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ < │ \ │   │   │   │       │   │   │   │   │   │   │
+      * │   │ < │ / │ [ │   │   │       │   │   │   │   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │   │   │ { │   │ [ │       │   │   │   │   │   │   │
+      * │   │   │   │ { │   │   │       │   │   │   │   │   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
                   ┌────┐                        ┌────┐
       *           │    ├────┐              ┌────┤    │
@@ -90,18 +89,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                     └────┘   └─────┘
       */
     [LEFT] = LAYOUT_split_3x6_3(
-        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_LPRN, XXXXXXX,
-        XXXXXXX, DK_LABK,  DK_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX,  XXXXXXX,  DK_LCBR,  XXXXXXX,  DK_LBRC,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                           XXXXXXX, TO(BASE), XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX
+        _______, _______,  _______,  _______,  _______,  _______,                _______, _______, _______, _______, DK_LPRN, _______,
+        _______, DK_LABK,  DK_SLSH,  DK_LBRC,  _______,  _______,                _______, _______, _______, _______, _______, _______,
+        _______, _______,  _______,  DK_LCBR,  _______,  _______,                _______, _______, _______, _______, _______, _______,
+                                           _______, TO(BASE), _______,        _______, TO(SYMBOL), _______
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
       * │   │   │   │   │   │   │       │   │   │   │   │ ) │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ > │ / │   │   │   │       │   │   │   │   │   │   │
+      * │   │ > │ \ │ ] │   │   │       │   │   │   │   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │   │   │ } │   │ ] │       │   │   │   │   │   │   │
+      * │   │   │   │ } │   │   │       │   │   │   │   │   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *           ┌────┐                        ┌────┐
       *           │    ├────┐              ┌────┤    │
@@ -110,49 +109,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                     └────┘   └─────┘
       */
     [RIGHT] = LAYOUT_split_3x6_3(
-        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_RPRN, XXXXXXX,
-        XXXXXXX, DK_RABK,  DK_SLSH,  XXXXXXX,  XXXXXXX,  XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX,  XXXXXXX,  DK_RCBR,  XXXXXXX,  DK_RBRC,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                          XXXXXXX, TO(BASE), XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX
+        _______, _______,  _______,  _______,  _______,  _______,                _______, _______, _______, _______, DK_RPRN, _______,
+        _______, DK_RABK,  DK_BSLS,  DK_RBRC,  _______,  _______,                _______, _______, _______, _______, _______, _______,
+        _______, _______,  _______,  DK_RCBR,  _______,  _______,                _______, _______, _______, _______, _______, _______,
+                                          _______, TO(BASE), _______,        _______, _______, _______
     ),
-     /*
-      * ┌────┬──────┬──────┬────────┬────────┬──────┐       ┌──────┬──────┬──────┬───────┬──────┬───┐
-      * │MSTP│ Play │ PRVS │ Next   │        │ CAPS │       │ PgDn │ PgUp │ INS  │ Print │      │Bsp│
-      * ├────┼──────┼──────┼────────┼────────┼──────┤       ├──────┼──────┼──────┼───────┼──────┼───┤
-      * │    │ HOME │ END  │ LCTRL+C│ LCTRL+V│      │       │ Left │ Down │ Up   │ Right │ Del  │   │
-      * ├────┼──────┼──────┼────────┼────────┼──────┤       ├──────┼──────┼──────┼───────┼──────┼───┤
-      * │Sft │ UNDO │ CUT  │ COPY   │ PASTE  │ RApp │       │ Vol- │ Vol+ │ Mute │       │ RGui │   │
-      * └────┴──────┴──────┴────────┴────────┴──────┘       └──────┴──────┴──────┴───────┴──────┴───┘
-      *                                ┌─────┐                       ┌─────┐
-      *                                │LCTRL├─────┐           ┌─────┤TO(3)│
-      *                                └─────┤TO(0)├───┐   ┌───┤TO(2)├─────┘
-      *                                      └─────┤ALT│   │SPC├─────┘
-      *                                            └───┘   └───┘
-      */
-    [MISC] = LAYOUT_split_3x6_3(
-        KC_MSTP, KC_MPLY,    KC_MPRV,   KC_MNXT,  XXXXXXX,  KC_CAPS,         KC_PGDN,   KC_PGUP,   KC_INS,   KC_PSCR,    XXXXXXX,    KC_BSPC,
-        XXXXXXX, KC_HOME,    KC_END,    C(KC_C),  C(KC_V),  XXXXXXX,         KC_LEFT,   KC_DOWN,   KC_UP,    KC_RGHT,    KC_DEL,   XXXXXXX,
-        KC_TAB,  KC_UNDO,    KC_CUT,    KC_COPY,  KC_PASTE, KC_APP,          KC_VOLD,   KC_VOLU,   KC_MUTE,    XXXXXXX,      XXXXXXX,  KC_RSFT,
-                                        KC_LCTL, TO(BASE), KC_LALT,        XXXXXXX, XXXXXXX, TO(FUNCTION)
-    ),
-     /*
-      * ┌──┬────┬────┬────┬────┬────┐       ┌────┬────┬────┬────┬─────┬─────┐
-      * │  │ F1 │ F2 │ F3 │ F4 │ F5 │       │ F6 │ F7 │ F8 │ F9 │ F10 │ F11 │
-      * ├──┼────┼────┼────┼────┼────┤       ├────┼────┼────┼────┼─────┼─────┤
-      * │  │    │    │    │    │    │       │    │    │    │    │     │ F12 │
-      * ├──┼────┼────┼────┼────┼────┤       ├────┼────┼────┼────┼─────┼─────┤
-      * │  │    │    │    │    │    │       │    │    │    │    │     │     │
-      * └──┴────┴────┴────┴────┴────┘       └────┴────┴────┴────┴─────┴─────┘
-      *                   ┌───┐                       ┌───┐
-      *                   │   ├─────┐           ┌─────┤   │
-      *                   └───┤TO(0)├───┐   ┌───┤     ├───┘
-      *                       └─────┤ENT│   │   ├─────┘
-      *                             └───┘   └───┘
-      */
-    [FUNCTION] = LAYOUT_split_3x6_3(
-        XXXXXXX, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                     KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,
-        XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     KC_F12,
-        XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,
-                                              XXXXXXX, TO(BASE), XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX
-    )
 };
