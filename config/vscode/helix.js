480e205714b400667ac67fef2371ffcd4d662e69
Simon Rask <33556894+SimonRask@users.noreply.github.com>
2024-08-11 20:34:07 +0200

    Change bind for `R`

--- config/vscode/helix.js
+++ config/vscode/helix.js
@@ -70,12 +70,15 @@ module.exports = {
 				before: true
 			}
 		}),
-		R: {
-			command: "modalEditor.paste",
-			args: {
-				replace: true
+		R: [
+			"modalEditor.delete",
+			{
+				command: "modalEditor.paste",
+				args: {
+					before: true
+				}
 			}
-		},
+		],
 		"`": "modalEditor.toLowerCase",
 		"~": "modalEditor.toUpperCase",
 		// Replace selections
@@ -194,13 +197,16 @@ module.exports = {
 					before: true
 				}
 			},
-			R: {
-				command: "modalEditor.paste",
-				args: {
-					register: "",
-					replace: true
+			R: [
+				"modalEditor.delete",
+				{
+					command: "modalEditor.paste",
+					args: {
+						register: "",
+						before: true
+					}
 				}
-			},
+			],
 			f: "workbench.action.quickOpen",
 			a: "editor.action.quickFix",
 			r: "editor.action.rename",
